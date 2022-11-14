0ca775fc2fb9ebb729cd86077b1d383bb1581725
idan chen <idanos@idans-MacBook-Pro-289.local>
2022-11-14 10:24:11 +0200

    adjusting api testing

--- backend/tests/posts.test.js
+++ backend/tests/posts.test.js
@@ -1,23 +1,20 @@
 const mongoose = require('mongoose')
 const request = require('supertest')
 const app = require('../server.js')
-beforeAll (done => {
-    done()
+const Post = require('../models/post_model')
+
+beforeAll (async () => {
+    await Post.remove()
 }) 
 
-afterAll (done => {
-    mongoose.connection.close()
-    done()
+afterAll (async ()=> {
+    await Post.remove()
+    mongoose.connection.close()   
 })
 
-describe("Testing Postagram API", ()=> {
+describe("Testing Postagram RESTful API", ()=> {
     const testMessage = "this is a test"
     const testSender = "111111"
-
-    test("Tests GET request to fetch all posts", async ()=>{
-        const response = await request(app).get('/post')
-        expect(response.statusCode).toEqual(200)
-    })
 
     test("Test POST request to add new post",async()=>{
         const response = await request(app).post('/post').send({
@@ -27,6 +24,21 @@ describe("Testing Postagram API", ()=> {
         expect(response.statusCode).toEqual(200)
         const newPost = response.body.post
         expect(newPost.message).toEqual(testMessage)
+        expect(newPost.sender).toEqual(testSender)
+    })
+
+    test("Tests GET request to fetch all posts", async ()=>{
+        const response = await request(app).get('/post')
+        expect(response.statusCode).toEqual(200)
+        expect(response.body[0].message).toEqual(testMessage)
+        expect(response.body[0].sender).toEqual(testSender)
+        expect(response.body[1]).toEqual(undefined)
+    })
+
+    test("Tests GET request to fetch all posts of a sender", async ()=>{
+        const response = await request(app).get('/post?sender='+testSender)
+        expect(response.statusCode).toEqual(200)
+        expect(response.body[0].sender).toEqual(testSender)
     })
 })
 
